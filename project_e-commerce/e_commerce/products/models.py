99849c587ae740189bcca9ef6ced5a2cae4e9863
Mohamed Hossam <80326979+mhdhossam@users.noreply.github.com>
2023-08-15 01:03:09 +0300

    Update models.py

--- project_e-commerce/e_commerce/products/models.py
+++ project_e-commerce/e_commerce/products/models.py
@@ -1,15 +1,17 @@
 from django.db import models
-from django.contrib.auth import get_user_model
-
-User =get_user_model()
+from user.models import CustomUser
 
 
+def category_image_path(instance, filename):
+    return f'product/category/icons/{instance.name}/{filename}'
 
-# Create your models here.
+def product_image_path(instance, filename):
+    return f'product/images/{instance.name}/{filename}'
+
 
 class ProductCategory(models.Model):
     name=models.CharField(['Category name'],max_length=100)
-    icon=models.ImageField(blank=True)
+    icon=models.ImageField(blank=True,upload_to=category_image_path)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
 
@@ -19,15 +21,28 @@ class ProductCategory(models.Model):
 
     def __str__(self) :
         return self.name
+    
 
 def get_default_product_category():
     return ProductCategory.objects.get_or_create(name='Others')[0]
+
 
 class Products(models.Model):
     # seller=models.ForeignKey(User,on_delete=models.CASCADE)
     category=models.ForeignKey('ProductCategory',on_delete=models.SET(get_default_product_category))
     name=models.CharField(max_length=200)
     description=models.TextField(blank=True)
+    price = models.DecimalField(('Price'), max_digits=10 , decimal_places=2 )
+    image = models.ImageField(blank=True,upload_to=product_image_path)
+    quantity = models.IntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+         ordering=['-created_at']
+
+    def __str__(self):
+        return self.name
 
 
 
